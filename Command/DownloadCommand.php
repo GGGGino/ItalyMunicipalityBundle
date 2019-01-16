808999d915c8a0921c975d5dddfd88c2650e02cb
David Ginanni <david.ginanni@gmail.com>
2019-01-17 00:26:23 +0100

    added command to warm the cache downloading datafrom ISTAT

---
+++ Command/DownloadCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GGGGino\ItalyMunicipalityBundle\Command;
+
+use GGGGino\ItalyMunicipalityBundle\Service\IstatPopulator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * @var IstatPopulator
+     */
+    private $istatPopulator;
+
+    public function __construct(IstatPopulator $istatPopulator)
+    {
+        $this->istatPopulator = $istatPopulator;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('ggggino:italy_municipality:download')
+            ->setDescription('Command to download the list from ISTAT');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lines = $this->istatPopulator->getLines();
+
+        $output->writeln("Trovati " . count($lines) . "comuni");
+    }
+}
