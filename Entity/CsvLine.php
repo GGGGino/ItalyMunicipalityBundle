f555e691bfaf047cb63abff6eea154ec16ecb9ad
David Ginanni <david.ginanni@gmail.com>
2019-01-31 10:21:15 +0100

    beautify return table

--- Entity/CsvLine.php
+++ Entity/CsvLine.php
@@ -43,29 +43,29 @@ class CsvLine
         if( empty($line[0]) && empty($line[1]) )
             throw new CsvLineNotValidException($line, "Csv line not valid");
 
-        $newSelf->codiceRegione = $line[0];
-        $newSelf->codiceUnitaTerritoriale = $line[1];
-        $newSelf->codiceProvincia = $line[2];
-        $newSelf->progressivoDelComune = $line[3];
-        $newSelf->codiceComuneAlfaumerico = $line[4];
-        $newSelf->denominazione = utf8_encode($line[5]);
-        $newSelf->denominazioneIta = utf8_encode($line[6]);
-        $newSelf->denominazioneAltraLingua = utf8_encode($line[7]);
-        $newSelf->codiceRipartizione = $line[8];
-        $newSelf->ripartizioneGeo = $line[9];
-        $newSelf->denomReg = utf8_encode($line[10]);
-        $newSelf->denomUnitaTerritoriale = utf8_encode($line[11]);
-        $newSelf->flagCapoluogo = $line[12];
-        $newSelf->siglaAutomob = $line[13];
-        $newSelf->codiceComuneNum = $line[14];
-        $newSelf->codiceComuneNum110 = $line[15];
-        $newSelf->codiceComuneNum107 = $line[16];
-        $newSelf->codiceComuneNum103 = $line[17];
-        $newSelf->codiceCatastale = $line[18];
-        $newSelf->popolazioneLegale = $line[19];
-        $newSelf->nuts1 = $line[20];
-        $newSelf->nuts2 = $line[21];
-        $newSelf->nuts3 = $line[22];
+        $newSelf->codiceRegione = trim($line[0]);
+        $newSelf->codiceUnitaTerritoriale = trim($line[1]);
+        $newSelf->codiceProvincia = trim($line[2]);
+        $newSelf->progressivoDelComune = trim($line[3]);
+        $newSelf->codiceComuneAlfaumerico = trim($line[4]);
+        $newSelf->denominazione = utf8_encode(trim($line[5]));
+        $newSelf->denominazioneIta = utf8_encode(trim($line[6]));
+        $newSelf->denominazioneAltraLingua = utf8_encode(trim($line[7]));
+        $newSelf->codiceRipartizione = trim(trim($line[8]));
+        $newSelf->ripartizioneGeo = trim(trim($line[9]));
+        $newSelf->denomReg = utf8_encode(trim($line[10]));
+        $newSelf->denomUnitaTerritoriale = utf8_encode(trim($line[11]));
+        $newSelf->flagCapoluogo = trim($line[12]);
+        $newSelf->siglaAutomob = trim($line[13]);
+        $newSelf->codiceComuneNum = trim($line[14]);
+        $newSelf->codiceComuneNum110 = trim($line[15]);
+        $newSelf->codiceComuneNum107 = trim($line[16]);
+        $newSelf->codiceComuneNum103 = trim($line[17]);
+        $newSelf->codiceCatastale = trim($line[18]);
+        $newSelf->popolazioneLegale = trim($line[19]);
+        $newSelf->nuts1 = trim($line[20]);
+        $newSelf->nuts2 = trim($line[21]);
+        $newSelf->nuts3 = trim($line[22]);
 
         return $newSelf;
     }
